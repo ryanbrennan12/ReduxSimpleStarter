e59aceec845f595fa93de9c5a3d86eb0896b0358
Ryan Brennan <ryanbrennanbc1@gmail.com>
2018-08-19 14:48:34 -0700

    first commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+//function that will return array of books
+export default function () {
+  return [
+    { title: 'Javascrip the good parts'},
+    { title: 'Harry Potter'},
+    { title: 'Dark Tower'},
+    { title: ' Eloquent JS'},
+  ]
+}
